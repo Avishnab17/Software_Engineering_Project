223d100baa90244ac1297522ebfc98064599eb0f
Avishna <bootunavishna5@gmail.com>
2022-03-07 23:04:37 +0400

    Linear equation solve

--- linear_Equation.h
+++ linear_Equation.h
@@ -2,9 +2,27 @@
 #define LINEAR_EQUATION_FILE
 #include <stdlib.h>
 
+float solve(float a, float b)
+{
+    return -(b/a);
+}
+
 void linearequation()
 {
-
+    float a=0, b=0;
+    float solution;
+    printf("Enter value of a : ");
+    scanf("%f",&a);
+    printf("Enter value of b : ");
+    scanf("%f",&b);
+    while(a==0){
+        printf("There is no solution for this linear equation!");
+        printf("Enter value of a again : ");
+        scanf("%f",&a);
+    }
+    solution = solve(a,b);
+    printf("Value of x is : %f", solution);
+    return 0;
 }
 
 #endif
