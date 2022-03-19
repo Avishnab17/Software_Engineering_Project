9959ecd4131f3371fc2c2468f004cad02de7ed29
Avishna <bootunavishna5@gmail.com>
2022-03-19 10:16:21 +0400

    linear

--- linear_Graph.h
+++ linear_Graph.h
@@ -1,38 +1,12 @@
 #ifndef LINEAR_GRAPH_FILE
 #define LINEAR_GRAPH_FILE
-<<<<<<< HEAD
-#include <stdio.h>  
-#include <math.h>  
-=======
 
-#include <stdio.h>
-#include <math.h>
->>>>>>> 05ed53a34415447fe871ed076332fabeb5e928f4
 
-#define WIDTH 60
-#define HEIGHT 30
-#define X WIDTH/2
-#define Y HEIGHT/2
-#define XMAX WIDTH-X-1
-#define XMIN -(WIDTH-X)
-#define YMAX HEIGHT-Y
-#define YMIN -(HEIGHT-Y)+1
-
-char Grid[HEIGHT][WIDTH];
-
-<<<<<<< HEAD
-int plot(int x, int y);
-void init_grid(void);
-void show_grid(void);
-
-=======
 int Plot(int x, int y);
-void Init_grid(void);
 void Show_grid(void);
 
 
 
->>>>>>> 05ed53a34415447fe871ed076332fabeb5e928f4
 /* Set "pixel" at specific coordinates */
 int Plot(int x, int y)
 {
