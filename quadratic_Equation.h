296f73fb4c9e7cfb9eac4122d596cbddcde08919
Yashi <yashimunogee@gmail.com>
2022-03-20 11:53:27 +0400

    changes in output of quadratic

--- quadratic_Equation.h
+++ quadratic_Equation.h
@@ -12,12 +12,12 @@ int quadraticequation()
     blue();
     printf("   a*x^2+b*x+c   \n"); 
     cyan();
-    printf("Enter value of a:\n");  
-    scanf("%f", &a);  
-    printf("Enter value of b:\n"); 
-    scanf("%f", &b);  
-    printf("Enter value of c:\n");
-    scanf("%f",  &c); 
+    printf("Enter value of a: ");  
+    scanf("%f",&a);  
+    printf("\nEnter value of b: "); 
+    scanf("%f",&b);  
+    printf("\nEnter value of c: ");
+    scanf("%f",&c); 
   
   
     if(a == 0)  
